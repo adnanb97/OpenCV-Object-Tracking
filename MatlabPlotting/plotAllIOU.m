55f936176c66a3868f470d72ed5e03b82809dfa3
abrdanin <b.adnan33@gmail.com>
2019-06-19 01:34:16 +0200

    Added Center Location Error Benchmark

--- MatlabPlotting/plotAllIOU.m
+++ MatlabPlotting/plotAllIOU.m
@@ -1,13 +1,13 @@
 clc;
 clear all; 
 videoName = '06-brzoZaklanjanje';
-fileID1 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4boostingMETRIKA2HSV.txt'),'r');
-fileID2 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4csrtMETRIKA2HSV.txt'),'r');
-fileID3 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4kcfMETRIKA2HSV.txt'),'r');
-fileID4 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4medianflowMETRIKA2HSV.txt'),'r');
-fileID5 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4milMETRIKA2HSV.txt'),'r');
-fileID6 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4mosseMETRIKA2HSV.txt'),'r');
-fileID7 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4tldMETRIKA2HSV.txt'),'r');
+fileID1 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4boostingMETRIKA5HSV.txt'),'r');
+fileID2 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4csrtMETRIKA5HSV.txt'),'r');
+fileID3 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4kcfMETRIKA5HSV.txt'),'r');
+fileID4 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4medianflowMETRIKA5HSV.txt'),'r');
+fileID5 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4milMETRIKA5HSV.txt'),'r');
+fileID6 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4mosseMETRIKA5HSV.txt'),'r');
+fileID7 = fopen(strcat(strcat('..\BenchmarkResults\Results', videoName),'.mp4tldMETRIKA5HSV.txt'),'r');
 
 formatSpec = '%f';
 boosting = fscanf(fileID1, formatSpec);
@@ -29,8 +29,8 @@ plot(t, mosse,  'DisplayName','mosse');
 plot(t, tld,  'DisplayName','tld');
 legend
 xlabel('Frame');
-ylabel('Maximum Success Rate');
-title('Benchmark results for each algorithm with HSV comparison - Maximum Success Rate');
+ylabel('Center Location Error');
+title('Benchmark results for each algorithm with HSV comparison - Center Location Error');
 hold off;
 fclose(fileID1);
 fclose(fileID2);
